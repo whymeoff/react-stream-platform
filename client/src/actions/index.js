aa03d68900aae6242394dccf6bd02e0da2c5fea4
Mark <50715288+whymeoff@users.noreply.github.com>
2020-05-23 23:04:13 +0300

    Setup reducers and actions

---
+++ client/src/actions/index.js
@@ -0,0 +1,54 @@
+import streams from '../api/streams'
+import history from '../history'
+
+export const signIn = (userId) => {
+    return {
+        type: 'SIGN_IN',
+        payload: {
+            userId
+        }
+    }
+}
+
+export const signOut = () => {
+    return {
+        type: 'SIGN_OUT'
+    }
+}
+
+export const createStream = formValues => async (dispatch, getState) => {
+    const { userId } = getState().auth
+    const res = await streams.post('/streams', { ...formValues, userId })
+
+    dispatch({ type: 'CREATE_STREAM', payload: res.data })
+
+    history.push('/')
+}
+
+export const fetchStreams = () => async dispatch => {
+    const res = await streams.get('/streams')
+
+    dispatch({ type: 'FETCH_STREAMS', payload: res.data })
+}
+
+export const fetchStream = id => async dispatch => {
+    const res = await streams.get(`/streams/${id}`)
+
+    dispatch({ type: 'FETCH_STREAM', payload: res.data })
+}
+
+export const editStream = (id, formValues) => async dispatch => {
+    const res = await streams.patch(`/streams/${id}`, formValues)
+
+    dispatch({ type: 'EDIT_STREAM', payload: res.data })
+
+    history.push('/')
+}
+
+export const deleteStream = id => async dispatch => {
+    await streams.delete(`/streams/${id}`)
+
+    dispatch({ type: 'DELETE_STREAM', payload: id })
+
+    history.push('/')
+}
