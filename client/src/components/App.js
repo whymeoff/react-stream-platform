078d4ca02667c47a6d6971da1ed9fc345f17ecd4
Mark <50715288+whymeoff@users.noreply.github.com>
2020-05-24 00:18:03 +0300

    Created video player

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Router, Route } from 'react-router-dom'
+import { Router, Route, Switch } from 'react-router-dom'
 import StreamList from './streams/StreamList'
 import StreamCreate from './streams/StreameCreate'
 import StreamEdit from './streams/StreamEdit'
@@ -13,11 +13,13 @@ const App = () => {
         <div className="ui container">
             <Router history={history}>
                 <Header />
-                <Route path="/" exact component={StreamList} />
-                <Route path="/streams/new" exact component={StreamCreate} />
-                <Route path="/streams/edit/:id" exact component={StreamEdit} />
-                <Route path="/streams/delete/:id" exact component={StreamDelete} />
-                <Route path="/streams/show" exact component={StreamShow} />
+                <Switch>
+                    <Route path="/" exact component={StreamList} />
+                    <Route path="/streams/new" exact component={StreamCreate} />
+                    <Route path="/streams/edit/:id" exact component={StreamEdit} />
+                    <Route path="/streams/delete/:id" exact component={StreamDelete} />
+                    <Route path="/streams/:id" exact component={StreamShow} />
+                </Switch>
             </Router>
         </div>
     )
--- client/src/components/streams/StreamShow.js
+++ client/src/components/streams/StreamShow.js
@@ -1,7 +1,66 @@
 import React from 'react'
+import { connect } from 'react-redux'
+import flv from 'flv.js'
+import { fetchStream } from '../../actions'
 
-const StreamShow = () => {
-    return <div>StreamShow</div>
+class StreamShow extends React.Component {
+    constructor(props) {
+        super(props)
+
+        this.videoRef = React.createRef()
+    }
+
+    componentDidMount() {
+        const { id } = this.props.match.params
+
+        this.props.fetchStream(id)
+        this.buildPlayer()
+    }
+
+    componentDidUpdate() {
+        this.buildPlayer()
+    }
+
+    componentDidUnmount() {
+        this.player.destroy()
+    }
+
+    buildPlayer = () => {
+        const { id } = this.props.match.params
+
+        if (this.player || !this.props.stream) {
+            return
+        }
+
+        this.player = flv.createPlayer({
+            type: 'flv',
+            url: `http://localhost:8000/live/${id}.flv`
+        })
+        this.player.attachMediaElement(this.videoRef.current)
+        this.player.load()
+    }
+
+    render() {
+        if (!this.props.stream) {
+            return <div>Loading...</div>
+        } 
+
+        const { title, description } = this.props.stream
+
+        return (
+            <div>
+                <video style={{ width: '100%' }} controls ref={this.videoRef} />
+                <h1>{title}</h1>
+                <h5>{description}</h5>
+            </div>
+        )
+    }
 }
 
-export default StreamShow
+const mapStateToProps = ({ streams }, ownProps) => {
+    return {
+        stream: streams[ownProps.match.params.id]
+    }
+}
+
+export default connect(mapStateToProps, { fetchStream })(StreamShow)
